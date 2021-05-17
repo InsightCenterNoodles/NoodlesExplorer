6293417a0ecfecdc275f104429655f873203a921
Nicholas Brunhart-Lupo <nicholasbl@icloud.com>
2021-05-17 12:17:21 -0600

    cleanups

--- src/delegates/delegates.cpp
+++ src/delegates/delegates.cpp
@@ -1,29 +1,6 @@
 #include "delegates.h"
 
-#include "instance_material/qinstancedmetalroughmaterial.h"
-#include "tabledata.h"
-
-#include <glm/gtc/type_ptr.hpp>
-
-#include <QColor>
-#include <QDebug>
-#include <QTreeWidgetItem>
-
-#include <QAbstractLight>
-#include <QAttribute>
-#include <QEntity>
-#include <QGeometry>
-#include <QGeometryRenderer>
-#include <QMaterial>
-
-
-#include <Qt3DRender/QBuffer>
-
-
-#include <Qt3DCore/QTransform>
-#include <Qt3DExtras/QMetalRoughMaterial>
-
-#include <QFile>
+#include <noo_common.h>
 
 QStringList to_qstringlist(std::vector<std::string_view> v) {
     QStringList ret;
